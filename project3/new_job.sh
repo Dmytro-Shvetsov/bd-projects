24350e59a9308dfc1c4c1b9ef30e5794cf1ea1a1
Roman Karpenko <romankar@login2.hpc.ut.ee>
2023-05-21 11:46:02 +0300

    added gbt

---
+++ project3/new_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J bd-project4
+#SBATCH --partition=main
+#SBATCH --nodes=1                 # node count
+#SBATCH --ntasks=1                # total number of tasks across all nodes
+#SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=128G
+#SBATCH --time=47:59:59           # total run time limit (HH:MM:SS)
+
+module load openjdk/1.8.0_265-b01
+module load python/3.7.7
+source venv/bin/activate
+ 
+lscpu
+free -g -h -t
+echo '# nodes: ----------------'
+printenv SLURM_NNODES
+
+python3.7 pyml_preds.py 
